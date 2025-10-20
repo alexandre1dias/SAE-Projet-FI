8f6235c51dc1b1e1811e4bbe407bc41be4f91fd4
alexandre1dias <alexandre.dias@etu.univ-orleans.fr>
2025-10-20 14:39:16 +0200

    début de la page connexion

--- monApp/forms.py
+++ monApp/forms.py
@@ -1,32 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, HiddenField, FloatField, IntegerField, PasswordField
-from wtforms.validators import DataRequired
-from . models import User, Auteur
-from hashlib import sha256
-from monApp import db
-
-class FormAuteur(FlaskForm):
-    idA=HiddenField('idA')
-    Nom = StringField ('Nom', validators =[DataRequired()])
-
-class FormLivre(FlaskForm):
-    idL=HiddenField("idL")
-    Prix = FloatField ('Prix', validators =[DataRequired()])
-    Titre = StringField("Titre", validators =[DataRequired()])
-    Url = StringField("Url")
-    Img = StringField("Img")
-    auteur_id = IntegerField("auteur_id")
+from wtforms import StringField, HiddenField, PasswordField
 
 
 class LoginForm(FlaskForm):
     Login = StringField ('Identifiant')
     Password = PasswordField ('Mot de passe')
     next = HiddenField()
-    def get_authenticated_user(self):
-        unUser = db.session.get(User, self.Login.data)
-        if unUser is None:
-            return None
-        m = sha256 ()
-        m.update(self.Password.data.encode())
-        passwd = m.hexdigest()
-        return unUser if passwd == unUser.Password else None
+
+ 
--- monApp/views.py
+++ monApp/views.py
@@ -2,6 +2,7 @@ from .app import app, db
 from flask import render_template, request,url_for , redirect
 from config import TITLE
 from flask_login import logout_user, login_user, login_required
+from .forms import LoginForm
 
 
 @app.route("/")
@@ -16,4 +17,19 @@ def about():
 @app.route("/contact/")
 def contact():
     return render_template("contact.html",title=TITLE+"- Conctact")
+
+#Vues pour le login 
+@app.route ("/login/", methods =("GET","POST" ,))
+def login():
+    unForm = LoginForm()
+    unUser=None
+    if not unForm.is_submitted():
+        unForm.next.data = request.args.get('next')
+    elif unForm.validate_on_submit():
+        unUser = unForm.get_authenticated_user()
+        if unUser:
+            login_user(unUser)
+            next = unForm.next.data or url_for("index",name=unUser.Login)
+            return redirect(next)
+    return render_template ("login.html",form=unForm)
 
