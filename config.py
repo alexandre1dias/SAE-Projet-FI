4cf07e44e15e8a2ac6c706198960ddd3bb77d0b0
Kurucelik Erman <o22401659@or-iut-i002-f28.iut45.univ-orleans.fr>
2025-11-06 09:25:30 +0100

    staged changes

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ SECRET_KEY = "2lzUl{$*D6#`8uXqlU."
 TITLE = "Cercle d'escrime Blois"
 BOOTSTRAP_SERVE_LOCAL = True
 
-LOGIN="dias"
-PASSWD="dias"
+LOGIN="kurucelik"
+PASSWD="kurucelik"
 SERVEUR="servinfo-maria"
-BD="DBdias"
+BD="DBkurucelik"
 SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{LOGIN}:{PASSWD}@{SERVEUR}/{BD}'
