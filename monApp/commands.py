fa81bfcfb63e498ee91d5f3f579062057a4369a9
Kurucelik Erman <o22401659@or-iut-i002-f05.iut45.univ-orleans.fr>
2026-02-09 11:55:35 +0100

    modification de mdp possible pour l'admin

---
+++ monApp/commands.py
@@ -0,0 +1,24 @@
+import click
+from .app import app, db
+from .modelBD import AdminBD
+from werkzeug.security import generate_password_hash
+
+@app.cli.command()
+@click.argument('email')
+@click.argument('new_password')
+def change_admin_password(email, new_password):
+    """Change le mot de passe d'un administrateur via le terminal."""
+    
+    # recherche de l'admin par son mail
+    admin = AdminBD.query.filter_by(email=email).first()
+    
+    if admin:
+        hash_mdp = generate_password_hash(new_password, method='pbkdf2:sha256')
+        
+        # mise à jour du champ mdp_hash 
+        admin.mdp_hash = hash_mdp
+        db.session.commit()
+        
+        print(f"Succès : Le mot de passe pour l'admin '{email}' a été mis à jour.")
+    else:
+        print(f"Erreur : Aucun administrateur trouvé avec l'email '{email}'.")
